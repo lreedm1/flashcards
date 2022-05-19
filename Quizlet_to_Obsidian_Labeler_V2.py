300583e65dd56cfe0db0e228a3790d9b54188dca
lreedm1 <lreedm1@gmail.com>
2022-05-19 10:59:49 -0500

    Formatting Improvements

--- Quizlet_to_Obsidian_Labeler_V2.py
+++ Quizlet_to_Obsidian_Labeler_V2.py
@@ -6,127 +6,127 @@ dataset = "/Users/reed/Documents/Nightly/bio test set.txt"
 directory = "/Users/reed/Library/Mobile Documents/iCloud~md~obsidian/Documents/biology test"
 
 def make_directory(file):
-    def titlecase_plus(text):
-        text = text.replace('/', ' or ')
-        text = text.replace('\\', ' or ')
-        split_on_space = text.split(' ')
-        acronyms = []
-        for i in split_on_space: 
-            for j in i:
-                if j.isupper():
-                    length = len(i)
-                    start = text.find(i)
-                    acronyms.append([start, start + length, i])
-                    break
-            
-        string_titlecase = titlecase(text)
-        for i in acronyms:
-            string_titlecase = string_titlecase[:i[0]] + i[2] + string_titlecase[i[1]:]
+	def titlecase_plus(text):
+		text = text.replace('/', ' or ')
+		text = text.replace('\\', ' or ')
+		split_on_space = text.split(' ')
+		acronyms = []
+		for i in split_on_space:
+			for j in i:
+				if j.isupper():
+					length = len(i)
+					start = text.find(i)
+					acronyms.append([start, start + length, i])
+					break
 
-        return string_titlecase
-    with open(file , 'r') as f:
-        contents = f.read()
-    contents = contents.replace(';;;;;', ';;;  ')
-    contents = contents.split(";;;")
-    contents = [x.split(';;') for x in contents]
-    
-    cards = []
-    for i in contents:
-        try:
-            for j in i:
-                j = j.strip()
-            if len(i[0]) > 1 and len(i[1]) > 1:
-                i[0] = titlecase_plus(i[0])
-                i[1] = i[1][0].upper() + i[1][1:]
-                if i[1][-1] == '.':
-                    i[1] = i[1][:-1]
-                cards.append(i)
-        except IndexError:
-            print(f'{i} improperly formatted')
-    print("Terms created")
-    return cards
+		string_titlecase = titlecase(text)
+		for i in acronyms:
+			string_titlecase = string_titlecase[:i[0]] + i[2] + string_titlecase[i[1]:]
+
+		return string_titlecase
+	with open(file , 'r') as f:
+		contents = f.read()
+	contents = contents.replace(';;;;;', ';;;  ')
+	contents = contents.split(";;;")
+	contents = [x.split(';;') for x in contents]
+
+	cards = []
+	for i in contents:
+		try:
+			for j in i:
+				j = j.strip()
+			if len(i[0]) > 1 and len(i[1]) > 1:
+				i[0] = titlecase_plus(i[0])
+				i[1] = i[1][0].upper() + i[1][1:]
+				if i[1][-1] == '.':
+					i[1] = i[1][:-1]
+				cards.append(i)
+		except IndexError:
+			print(f'{i} improperly formatted')
+	print("Terms created")
+	return cards
 
 def write_cards(cards, directory):
-    print("writing cards")
-    for i in cards:
-        filename = f'{directory}/{i[0]}.md'
-        write(filename, i[1])
-    print('cards written to disk')
+	print("writing cards")
+	for i in cards:
+		filename = f'{directory}/{i[0]}.md'
+		write(filename, i[1])
+	print('cards written to disk')
 
 def format_cards(card_pairs, connections, scores):
-    def format_definition(outbound_connections, card_pair, card_pairs):
-        if len(card_pair[0][1]) != 1:
-            statement = '**Definition 1:**\n'
-            formatted = statement + card_pair[1][1][0]
-            unformatted =  ' ' + ' ' * len(statement) + card_pair[0][1][0]
-            for a in range(len(card_pair[0][1])-1):
-                statement = f'\n\n**Definition {a+2}:**\n'
-                formatted += statement + card_pair[1][1][a+1]
-                unformatted += ' ' * len(statement) + card_pair[0][1][a+1]
-        else:
-            formatted = card_pair[1][1][0]
-            unformatted = ' ' + card_pair[0][1][0]
-        if len(outbound_connections) == 0:
-            return formatted
-        sorted_connections = []
-        for i in outbound_connections:
-            sorted_connections.append([sorted(card_pairs[i][0][0],key = len, reverse = True), card_pairs[i][1][0]])
-        sorted_connections = sorted(sorted_connections, key = lambda x: len(x[0][0]), reverse = True)
-        
-        for i in sorted_connections:    
-            for j in i[0]:
-                location = unformatted.find(j)
-                if location != -1:
-                    length = len(j) -2
-                    statement = f'[[{i[1]}|{formatted[location:location + length]}]]'
-                    formatted = formatted[:location] + statement + formatted[location + length:]
-                    unformatted = unformatted[:location] + ' ' * len(statement) + unformatted[location + length:]
-        return formatted
-    print("formatting cards")
-    cards_print = []
-    for a, card_pair in enumerate(card_pairs):
-        outbound, inbound = connections[a]
-        formatted_text = format_definition(outbound, card_pair, card_pairs)
-        formatted_text += f'\n\nScore - {scores[a][1]}'
-        if len(inbound) != 0:
-            inbound_print = []
-            for j in inbound:
-                inbound_print.append([card_pairs[j][1], scores[j][1]])
-            inbound_print = sorted(inbound_print, key=lambda x: x[1], reverse=True)
-            for j in inbound_print:
-                formatted_text += f'\n[[{j[0][0]}]] - {j[1]}'
-        cards_print.append([card_pair[1][0], formatted_text])
-    return cards_print
-        
+	def format_definition(outbound_connections, card_pair, card_pairs):
+		if len(card_pair[0][1]) != 1:
+			statement = '**Definition 1:**\n'
+			formatted = statement + card_pair[1][1][0]
+			unformatted =  ' ' + ' ' * len(statement) + card_pair[0][1][0]
+			for a in range(len(card_pair[0][1])-1):
+				statement = f'\n\n**Definition {a+2}:**\n'
+				formatted += statement + card_pair[1][1][a+1]
+				unformatted += ' ' * len(statement) + card_pair[0][1][a+1]
+		else:
+			formatted = card_pair[1][1][0]
+			unformatted = ' ' + card_pair[0][1][0]
+		if len(outbound_connections) == 0:
+			return formatted
+		sorted_connections = []
+		for i in outbound_connections:
+			sorted_connections.append([sorted(card_pairs[i][0][0],key = len, reverse = True), card_pairs[i][1][0]])
+		sorted_connections = sorted(sorted_connections, key = lambda x: len(x[0][0]), reverse = True)
+
+		for i in sorted_connections:
+			for j in i[0]:
+				location = unformatted.find(j)
+				if location != -1:
+					length = len(j) -2
+					statement = f'[[{i[1]}|{formatted[location:location + length]}]]'
+					formatted = formatted[:location] + statement + formatted[location + length:]
+					unformatted = unformatted[:location] + ' ' * len(statement) + unformatted[location + length:]
+		return formatted
+	print("formatting cards")
+	cards_print = []
+	for a, card_pair in enumerate(card_pairs):
+		outbound, inbound = connections[a]
+		formatted_text = format_definition(outbound, card_pair, card_pairs)
+		formatted_text += f'\n\nScore - {scores[a][1]}'
+		if len(inbound) != 0:
+			inbound_print = []
+			for j in inbound:
+				inbound_print.append([card_pairs[j][1], scores[j][1]])
+			inbound_print = sorted(inbound_print, key=lambda x: x[1], reverse=True)
+			for j in inbound_print:
+				formatted_text += f'\n[[{j[0][0]}]] - {j[1]}'
+		cards_print.append([card_pair[1][0], formatted_text])
+	return cards_print
+
 def stop_append(stop):
-    stop.append(timeit.default_timer())
+	stop.append(timeit.default_timer())
 
 def print_times(stop):
-        for i in range(1, len(stop)):
-            print(f'{round(stop[i] - stop[i-1],2)} seconds')
-        print(f'total: {round(stop[-1] - stop[0],2)} seconds')
+		for i in range(1, len(stop)):
+			print(f'{round(stop[i] - stop[i-1],2)} seconds')
+		print(f'total: {round(stop[-1] - stop[0],2)} seconds')
 
 def main():
-    stop = []
-    stop_append(stop)
+	stop = []
+	stop_append(stop)
 
-    cards = make_directory(dataset)
-    stop_append(stop)
+	cards = make_directory(dataset)
+	stop_append(stop)
 
-    card_pairs = normalize(cards)
-    stop_append(stop)
-    
-    connections = make_connections(card_pairs)
-    stop_append(stop)
+	card_pairs = normalize(cards)
+	stop_append(stop)
 
-    scores = weight_cards(connections, card_pairs, directory)
-    stop_append(stop)
+	connections = make_connections(card_pairs)
+	stop_append(stop)
 
-    cards = format_cards(card_pairs, connections, scores)
-    stop_append(stop)
+	scores = weight_cards(connections, card_pairs, directory)
+	stop_append(stop)
 
-    write_cards(cards, directory)
-    stop_append(stop)
+	cards = format_cards(card_pairs, connections, scores)
+	stop_append(stop)
 
-    print_times(stop)
+	write_cards(cards, directory)
+	stop_append(stop)
+
+	print_times(stop)
 main()
--- card_analysis.py
+++ card_analysis.py
@@ -1,94 +1,94 @@
 punctuation_list = '!#$%&()*+,-./:;<=>?@[\]^_`{|}~'
 
 def normalize(dictionary):
-    def remove_duplicates(pairs):
-        for a in range(len(pairs)):
-            if pairs[a] == 0:
-                continue
-            for j in pairs[a][0]:
-                for c in range(len(pairs)):
-                    if pairs[c] == 0 or c == a:
-                        continue
-                    if j not in pairs[c][0]:
-                        continue
-                    for d, l in enumerate(pairs[a][0]):
-                        if l not in pairs[c][0]:
-                            pairs[c] = (pairs[c][0] + [l], pairs[c][1] + [pairs[a][1][d]])
-                    pairs[a] = 0
-                    a = c
-        pairs = [i for i in pairs if i != 0]
-        return pairs
-    def split_alternates(string):
-        if "(" in string:
-            x = [string.find("("), string.find(")")]
-            string = [" " + string[:x[0]].strip() + " " + string[x[1]+1:].strip() + " ",
-            " " + string[x[0]+1:x[1]].strip() + " "]
-            string.append('')
-        else:
-            string = [string, '']
-        return string
-    def normalize_spelling(input):
-        string = input.lower().strip()
-        string = " " + string + " "
-        illegal_characters = punctuation_list.replace('-', '')
-        for j in illegal_characters:
-            string = string.replace(j, " ")
-        return string
-    print("Normalizing terms...")
-    card_pairs = []
-    for a, i in enumerate(dictionary):
-        new_terms = split_alternates(i[0])
-        new_terms[-1] = i[1] + "~"
-        new_terms = ([normalize_spelling(j) for j in new_terms], new_terms)
-        card_pairs.append(new_terms)
-    card_pairs = remove_duplicates(card_pairs)
-    for a, i in enumerate(card_pairs):
-        new_i = [[[],[]],[[],[]]]
-        for b, j in enumerate(i[1]):
-            if j[-1] == "~":
-                new_i[0][1].append(" " + i[0][b].strip() + " ")
-                new_i[1][1].append(" " + i[1][b][:-1] + " ")
-            else:
-                new_i[0][0].append(i[0][b])
-                new_i[1][0].append(i[1][b])
-        card_pairs[a] = new_i
-    for a, i in enumerate(card_pairs):
-            if len(i[1][0]) == 1:
-                i[1][0] = i[1][0][0]
-                continue
-            formatted_uncombined = sorted(i[1][0], key = len, reverse = True)
-            card_pairs[a][1][0] = formatted_uncombined[0].strip()
-            for i in formatted_uncombined[1:]:
-                card_pairs[a][1][0] += f' ({i.strip()})'
-    return card_pairs
+	def remove_duplicates(pairs):
+		for a in range(len(pairs)):
+			if pairs[a] == 0:
+				continue
+			for j in pairs[a][0]:
+				for c in range(len(pairs)):
+					if pairs[c] == 0 or c == a:
+						continue
+					if j not in pairs[c][0]:
+						continue
+					for d, l in enumerate(pairs[a][0]):
+						if l not in pairs[c][0]:
+							pairs[c] = (pairs[c][0] + [l], pairs[c][1] + [pairs[a][1][d]])
+					pairs[a] = 0
+					a = c
+		pairs = [i for i in pairs if i != 0]
+		return pairs
+	def split_alternates(string):
+		if "(" in string:
+			x = [string.find("("), string.find(")")]
+			string = [" " + string[:x[0]].strip() + " " + string[x[1]+1:].strip() + " ",
+			" " + string[x[0]+1:x[1]].strip() + " "]
+			string.append('')
+		else:
+			string = [string, '']
+		return string
+	def normalize_spelling(input):
+		string = input.lower().strip()
+		string = " " + string + " "
+		illegal_characters = punctuation_list.replace('-', '')
+		for j in illegal_characters:
+			string = string.replace(j, " ")
+		return string
+	print("Normalizing terms...")
+	card_pairs = []
+	for a, i in enumerate(dictionary):
+		new_terms = split_alternates(i[0])
+		new_terms[-1] = i[1] + "~"
+		new_terms = ([normalize_spelling(j) for j in new_terms], new_terms)
+		card_pairs.append(new_terms)
+	card_pairs = remove_duplicates(card_pairs)
+	for a, i in enumerate(card_pairs):
+		new_i = [[[],[]],[[],[]]]
+		for b, j in enumerate(i[1]):
+			if j[-1] == "~":
+				new_i[0][1].append(" " + i[0][b].strip() + " ")
+				new_i[1][1].append(" " + i[1][b][:-1] + " ")
+			else:
+				new_i[0][0].append(i[0][b])
+				new_i[1][0].append(i[1][b])
+		card_pairs[a] = new_i
+	for a, i in enumerate(card_pairs):
+			if len(i[1][0]) == 1:
+				i[1][0] = i[1][0][0]
+				continue
+			formatted_uncombined = sorted(i[1][0], key = len, reverse = True)
+			card_pairs[a][1][0] = formatted_uncombined[0].strip()
+			for i in formatted_uncombined[1:]:
+				card_pairs[a][1][0] += f' ({i.strip()})'
+	return card_pairs
 
 def make_connections(terms):
-    print("Searching for connections...")
-    connections = [[(),()] for i in range(len(terms))]
-    term_chunks = tuple("".join(i[0][0] + i[0][1]) for i in terms)
-    titles = tuple(i[0][0] for i in terms)
-    for a, i in enumerate(term_chunks):
-        for b, j in enumerate(titles):
-            for k in j:
-                if a != b and k in i:
-                    connections[a][0] += (b,)
-                    connections[b][1] += (a,)
-                    break
-    return connections
+	print("Searching for connections...")
+	connections = [[(),()] for i in range(len(terms))]
+	term_chunks = tuple("".join(i[0][0] + i[0][1]) for i in terms)
+	titles = tuple(i[0][0] for i in terms)
+	for a, i in enumerate(term_chunks):
+		for b, j in enumerate(titles):
+			for k in j:
+				if a != b and k in i and len(k) > 4:
+					connections[a][0] += (b,)
+					connections[b][1] += (a,)
+					break
+	return connections
 
 def weight_cards(connections, terms, directory, file_name='card stats.md'):
-    card_scores = sorted([(a,len(i[0]) *.1 + len(i[1])) for a,i in enumerate(connections)], key=lambda x: x[1], reverse=True)
-    max, min = card_scores[0][1], card_scores[-1][1]
-    card_scores = [[i[0],(i[1] - min) * 100 / (max - min)] for i in card_scores]
-    print_statement = ''
-    for a,i in enumerate(card_scores):
-        i = (i[0],round(i[1], 1)) if i[1] < 1 else (i[0],round(i[1]))
-        card_scores[a] = i
-        print_statement += f'{i[1]} - [[{terms[i[0]][1][0]}]]\n'
-    write(directory + "/" + file_name, print_statement)
-    card_scores.sort(key = lambda x: x[0])
-    return card_scores
+	card_scores = sorted([(a,len(i[0]) *.1 + len(i[1])) for a,i in enumerate(connections)], key=lambda x: x[1], reverse=True)
+	max, min = card_scores[0][1], card_scores[-1][1]
+	card_scores = [[i[0],(i[1] - min) * 100 / (max - min)] for i in card_scores]
+	print_statement = ''
+	for a,i in enumerate(card_scores):
+		i = (i[0],round(i[1], 1)) if i[1] < 1 else (i[0],round(i[1]))
+		card_scores[a] = i
+		print_statement += f'{i[1]} - [[{terms[i[0]][1][0]}]]\n'
+	write(directory + "/" + file_name, print_statement)
+	card_scores.sort(key = lambda x: x[0])
+	return card_scores
 
 def write(filename, statement):
-    with open(filename, 'w') as f:
-        f.write(statement)
+	with open(filename, 'w') as f:
+		f.write(statement)
